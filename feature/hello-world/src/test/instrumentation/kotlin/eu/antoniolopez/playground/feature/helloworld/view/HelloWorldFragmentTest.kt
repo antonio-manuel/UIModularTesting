672efac867ecdee1caf4e1a4ca33f87c3cc9d39a
antonio lopez <antonio.lopez@metropolis-lab.io>
2019-04-04 16:59:44 +0200

    Deleted old comments

--- feature/hello-world/src/test/instrumentation/kotlin/eu/antoniolopez/playground/feature/helloworld/view/HelloWorldFragmentTest.kt
+++ feature/hello-world/src/test/instrumentation/kotlin/eu/antoniolopez/playground/feature/helloworld/view/HelloWorldFragmentTest.kt
@@ -14,11 +14,6 @@ import org.junit.Test
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.singleton
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class HelloWorldFragmentTest : InstrumentationUnitTest() {
 
     private val mockPresenter : HelloWorldPresenter = mockk(relaxed = true)
