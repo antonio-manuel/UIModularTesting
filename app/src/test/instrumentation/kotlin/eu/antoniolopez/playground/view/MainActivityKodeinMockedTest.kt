aaff91ca41ddfcd1b9ffbbc1b83174280ecabbfa
antonio lopez <antonio.lopez@metropolis-lab.io>
2019-04-05 11:07:56 +0200

    Deleted unused dependencies

--- app/src/test/instrumentation/kotlin/eu/antoniolopez/playground/view/MainActivityKodeinMockedTest.kt
+++ app/src/test/instrumentation/kotlin/eu/antoniolopez/playground/view/MainActivityKodeinMockedTest.kt
@@ -1,6 +1,7 @@
 package eu.antoniolopez.playground.view
 
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -8,17 +9,33 @@ import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import eu.antoniolopez.playground.R
+import eu.antoniolopez.playground.di.appComponent
+import eu.antoniolopez.playground.presenter.MainPresenter
+import io.mockk.clearAllMocks
+import io.mockk.mockk
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.provider
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class MainActivityWithoutCoreViewTestingTest {
+class MainActivityKodeinMockedTest {
+
+    private val mockPresenter: MainPresenter = mockk(relaxed = true)
 
     @get:Rule
     var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Before
+    fun prepareKodein() {
+        clearAllMocks()
+        appComponent.addConfig {
+            bind<MainPresenter>(overrides = true) with provider { mockPresenter }
+        }
+    }
 
     @Test
     fun checkOpens() {
@@ -28,6 +45,7 @@ class MainActivityWithoutCoreViewTestingTest {
     @Test
     fun checkButtonText() {
         onView(withId(R.id.button_activity))
+            .perform(click())
             .check(matches(withText("Navigate")))
     }
 }
--- app/src/test/instrumentation/kotlin/eu/antoniolopez/playground/view/MainActivityTest.kt
+++ app/src/test/instrumentation/kotlin/eu/antoniolopez/playground/view/MainActivityTest.kt
@@ -1,7 +1,6 @@
 package eu.antoniolopez.playground.view
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
@@ -13,17 +12,22 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class SimpleAppActivityTest {
+class MainActivityTest {
 
     @get:Rule
-    var activityScenarioRule = activityScenarioRule<SimpleAppActivity>()
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun checkOpens() {
+        onView(withId(R.id.content))
+    }
 
     @Test
     fun checkButtonText() {
-        onView(withId(R.id.simple_button))
-            .perform(click())
+        onView(withId(R.id.button_activity))
             .check(matches(withText("Navigate")))
     }
 }
