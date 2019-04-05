220ab96932f5fea0e058aa619bb472efe6627db5
antonio lopez <antonio.lopez@metropolis-lab.io>
2019-04-05 10:57:18 +0200

    Rolled back unit tests to jupiter

--- infrastructure/core-testing/src/main/kotlin/eu/antoniolopez/playground/core/testing/UnitTest.kt
+++ infrastructure/core-testing/src/main/kotlin/eu/antoniolopez/playground/core/testing/UnitTest.kt
@@ -1,18 +1,16 @@
 package eu.antoniolopez.playground.core.testing
 
-import androidx.test.filters.SmallTest
 import eu.antoniolopez.playground.core.testing.threading.CoroutineContextForTest
 import eu.antoniolopez.playground.threading.APPLICATION_BG
 import eu.antoniolopez.playground.threading.APPLICATION_MAIN
 import io.mockk.clearAllMocks
 import org.junit.After
 import org.junit.Before
+import org.junit.jupiter.api.TestInstance
 
 @Suppress("IllegalIdentifier")
-@SmallTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class UnitTest {
-    val mocksCache = mutableListOf<Any>()
-
     @Before
     fun onBefore() {
         APPLICATION_MAIN = CoroutineContextForTest
